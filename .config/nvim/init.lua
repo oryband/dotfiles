8b1b421e362ae1b5b6aaf5f95be8abf09f209b77
Ory Band <oryband@users.noreply.github.com>
2021-10-17 17:27:32 +0300

    vim remove virtual text popup from lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -214,6 +214,14 @@ require('nvim-treesitter.configs').setup {
 
 -- LSP settings
 local nvim_lsp = require 'lspconfig'
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  signs = true,
+  underline = false,
+  update_in_insert = false,
+})
+
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
