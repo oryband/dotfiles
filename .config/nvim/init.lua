13c81a951cc6b2895fbe4992d6d5a4e5df929605
Ory Band <oryband@users.noreply.github.com>
2021-10-17 17:26:56 +0300

    vim set localleader to ,

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -97,7 +97,7 @@ require('lualine').setup{
 --Remap space as leader key
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 
 --Remap for dealing with word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
