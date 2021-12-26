681d99f2d8421f8ca8952c0d00bd677b5ec1eb35
Ory Band <oryband@users.noreply.github.com>
2021-12-26 13:13:51 +0200

    nvim install treesitter rainbow

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,6 +51,7 @@ require('packer').startup(function()
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'nvim-treesitter/playground'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'p00f/nvim-ts-rainbow'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
