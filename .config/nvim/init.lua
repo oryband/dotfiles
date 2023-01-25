9b1c9bd3f56cfd55e79f220a54b29f7e4deecfea
Ory Band <oryband@users.noreply.github.com>
2023-01-25 13:28:29 +0200

    nvim lock treesitter version ; readd textobjects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,8 +51,9 @@ require('packer').startup(function()
   use 'kyazdani42/nvim-web-devicons'
   use {'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
   use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use { 'paopaol/telescope-git-diffs.nvim', requires = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" }, }
+  use {'nvim-treesitter/nvim-treesitter', version = 'v0.8.1', run = ':TSUpdate'}
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
   use 'nvim-treesitter/playground'
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp'
