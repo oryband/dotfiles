39b5cfef3654f7d8c82c1ae626e308d22fc35304
Ory Band <oryband@users.noreply.github.com>
2023-03-26 15:37:05 +0300

    nvim: cosmetic format init.lua

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -20,20 +20,25 @@ return {
   },
   plugins = {
     { "dracula/vim", name = "dracula" },
-
     { "goolord/alpha-nvim", opts = function(_, opts) opts.section.header.val = {} end },
+    { "tpope/vim-surround", lazy = false },
+    { "tpope/vim-repeat", lazy = false },
 
-    { "tpope/vim-surround" },
-    { "tpope/vim-repeat" },
-
-    { "guns/vim-sexp", ft = { "clojure" }, config = function() vim.api.nvim_set_var("sexp_enable_insert_mode_mappings", 0) end, },
+    { "guns/vim-sexp", ft = { "clojure" }, config = function() vim.api.nvim_set_var("sexp_enable_insert_mode_mappings", 0) end },
     -- { "Olical/conjure", ft = { "clojure" } },
 
     { "nvim-treesitter/nvim-treesitter", ensure_installed = { "clojure", "lua", "markdown", "python" } },
-    { "nvim-treesitter/playground", lazy = false},
+    { "nvim-treesitter/playground", lazy = false },
     { "williamboman/mason-lspconfig.nvim", ensure_installed = { "clojure_lsp", "lua_language_server" } },
     { "jay-babu/mason-null-ls.nvim", ensure_installed = { "black", "jq", "stylua", "zprint" } },
-    { "jose-elias-alvarez/null-ls.nvim", config = function(config) local null_ls = require "null-ls" ; config.sources = { null_ls.builtins.formatting.zprint } ; return config end },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function(config)
+        local null_ls = require "null-ls"
+        config.sources = { null_ls.builtins.formatting.zprint }
+        return config
+      end,
+    },
     { "jay-babu/mason-nvim-dap.nvim", ensure_installed = { "python" } },
   },
 }
