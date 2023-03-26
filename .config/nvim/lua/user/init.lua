76127e14e6838071c817f867500ae0ae38a5d53a
Ory Band <oryband@users.noreply.github.com>
2023-03-26 15:37:23 +0300

    nvim add copilot

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -28,6 +28,14 @@ return {
     { "guns/vim-sexp", ft = { "clojure" }, config = function() vim.api.nvim_set_var("sexp_enable_insert_mode_mappings", 0) end },
     -- { "Olical/conjure", ft = { "clojure" } },
 
+    { "zbirenbaum/copilot.lua", config = function() require("copilot").setup { suggestion = { enabled = false }, panel = { enabled = false } } end, },
+    {
+      "zbirenbaum/copilot-cmp",
+      event = "InsertEnter",
+      dependencies = { "zbirenbaum/copilot.lua", },
+      config = function() require("copilot_cmp").setup { formatters = { insert_text = require("copilot_cmp.format").remove_existing } } end,
+    },
+
     { "nvim-treesitter/nvim-treesitter", ensure_installed = { "clojure", "lua", "markdown", "python" } },
     { "nvim-treesitter/playground", lazy = false },
     { "williamboman/mason-lspconfig.nvim", ensure_installed = { "clojure_lsp", "lua_language_server" } },
@@ -41,6 +49,23 @@ return {
       end,
     },
     { "jay-babu/mason-nvim-dap.nvim", ensure_installed = { "python" } },
+
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = { "zbirenbaum/copilot-cmp" },
+      opts = function(_, opts)
+        local cmp = require "cmp"
+        opts.sources = cmp.config.sources {
+          { name = "copilot", priority = 1000 },
+          { name = "nvim_lsp", priority = 900 },
+          { name = "luasnip", priority = 750 },
+          { name = "buffer", priority = 500 },
+          { name = "path", priority = 250 },
+        }
+
+        return opts
+      end,
+    },
   },
   polish = function()
     local opts = { silent = true }
