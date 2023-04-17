4ea574fe5e7598d3d3093a3fda1ed45f15315c9b
Ory Band <oryband@users.noreply.github.com>
2023-04-17 16:02:29 +0300

    nvim add copilot icon

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -60,6 +60,15 @@ return {
         return opts
       end,
     },
+
+    {
+      "onsails/lspkind.nvim",
+      opts = {
+        symbol_map = {
+          Copilot = "",
+        }
+      },
+    },
   },
   polish = function()
     local opts = { silent = true }
@@ -68,5 +77,7 @@ return {
     map("", "w", "<Plug>CamelCaseMotion_w", opts)
     map("", "e", "<Plug>CamelCaseMotion_e", opts)
     map("", "b", "<Plug>CamelCaseMotion_b", opts)
+
+    vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { link = "CmpItemKindDefault" })
   end,
 }
