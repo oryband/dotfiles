f8d2fa440d6344a25e777b20552a52f3cd49fa16
Ory Band <oryband@users.noreply.github.com>
2023-07-10 16:32:52 +0300

    nvim update copilot settings (remove existing)

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -71,12 +71,17 @@ return {
       },
     },
 
-    { "zbirenbaum/copilot.lua", config = function() require("copilot").setup { suggestion = { enabled = false }, panel = { enabled = false } } end, },
+    {
+      "zbirenbaum/copilot.lua",
+      config = function() require("copilot").setup { suggestion = { enabled = false }, panel = { enabled = false } } end,
+    },
     {
       "zbirenbaum/copilot-cmp",
       event = "InsertEnter",
-      dependencies = { "zbirenbaum/copilot.lua", },
-      config = function() require("copilot_cmp").setup { formatters = { insert_text = require("copilot_cmp.format").remove_existing } } end,
+      dependencies = { "zbirenbaum/copilot.lua" },
+      config = function()
+        require("copilot_cmp").setup { formatters = { insert_text = require("copilot_cmp.format").remove_existing } }
+      end,
     },
 
     {
